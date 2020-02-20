ef2c787e76ed5431971b0e766f8409938e92a21c
Noah Broyles <noah@javamate.net>
2020-02-20 11:31:46 -0500

    Added some real CODE

--- HelloWorldInPython/Src/main.py
+++ HelloWorldInPython/Src/main.py
@@ -5,3 +5,8 @@ Created on Feb 20, 2020
 '''
 
 print('Hello World')
+
+# Hello world is awesome right? But let's actually DO something. 
+name = input("Hey man what's your name? ")     # See, here we can use " because we also use ' in the string. WAY cooler than Java.
+print('Well, hello', name +'!')
+# See? isn't python the best?
